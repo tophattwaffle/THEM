87a4125d1576c8d80cbb7be88c1b5a391e102b04
Douglas Hoogland <tophattwaffle@gmail.com>
2023-01-28 22:32:23 -0600

    Disabled drafts only

--- Lib/EtsyAPIGlobalVars.ps1
+++ Lib/EtsyAPIGlobalVars.ps1
@@ -4,7 +4,7 @@ Write-Host "Loading GlobalVars..." -ForegroundColor Magenta
 These are variables that you can change to fit your needs.
 #>
 $global:dontRefreshOnLoad = $false
-$global:DraftsOnly = $true
+$global:DraftsOnly = $false
 $global:redirectURL = "https://www.tophattwaffle.com/auth.php"
 $global:saveLocation = "$([Environment]::GetFolderPath("MyDocuments"))\EtsyAPI"
 
